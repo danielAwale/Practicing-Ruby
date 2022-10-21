2574cdd87096dfe16fb7ef2989d3847b136fe0d4
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-10-21 23:52:00 +0000

    contain a file to exercise classes and object

---
+++ classobject.rb
@@ -0,0 +1,18 @@
+class Book 
+  attr_accessor :title, :author, :pages
+
+
+end
+
+book1 = Book.new()
+book1.title = "Harry Potter"
+book1.author = "JK Rowling"
+book.pages = 400
+
+book2 = Book.new()
+book2.title = "Lord of the Rings"
+book2.author = "J.R. TOLKIEN"
+book3.author = 600 
+
+class Person
+end
