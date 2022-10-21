f0ae7a416a7a377b28edf494d31eff6a1dc60ec0
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-10-21 21:00:48 +0000

    refactor code to add float

--- Calculator.rb
+++ Calculator.rb
@@ -1,6 +1,6 @@
 puts "Enter a number: "
-num1 = gets.chomp.to_i
+num1 = gets.chomp.to_f
 puts "Enter another number: "
-num2 = gets.chomp.to_i
+num2 = gets.chomp.to_f
 
 puts "Adding these numbers will give us #{num1 + num2}"
